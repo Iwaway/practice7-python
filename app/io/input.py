1030f8942f4e035451c8016e1fae3b5a588bf49c
Iwaway <belokonegor2000@gmail.com>
2024-03-17 19:59:17 +0200

    io funcs creation

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def input_text_from_console():
+    pass
+
+def read_text_from_file(filename):
+    pass
+
+def read_text_from_file_pd(filename):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def output_text_to_console(text):
+    pass
+
+def write_text_to_file(text, filename):
+    pass
+
+def write_text_to_file_pd(text, filename):
+    pass
